0011c77027138c4d16e9d3b47fc4fce41c64729f
Ilia <tereshchen@sfedu.ru>
2023-12-14 19:50:19 +0300

    Created Classes Team, User, Order, SimpleUser

---
+++ src/main/java/com/teamsearch/teamsearchbackend/Entities/Team.java
@@ -0,0 +1,19 @@
+package com.teamsearch.teamsearchbackend.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "teams")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/main/java/com/teamsearch/teamsearchbackend/Entities/User.java
@@ -0,0 +1,20 @@
+package com.teamsearch.teamsearchbackend.Entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
