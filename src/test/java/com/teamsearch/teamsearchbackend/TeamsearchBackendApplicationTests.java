08dc3332a7bd195661cd391671013a09106131a8
Ilia <tereshchen@sfedu.ru>
2023-12-14 19:52:26 +0300

    Created TestsApplication

---
+++ src/test/java/com/teamsearch/teamsearchbackend/TeamsearchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsearch.teamsearchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsearchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
